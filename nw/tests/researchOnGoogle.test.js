8fcfd448e131dc96af3de61ebfcbdf192e5dce8a
Mathieu Breton <mathieu.breton@js-republic.com>
2016-02-04 12:01:58 +0100

    Add complexe command

--- nw/tests/researchOnGoogle.test.js
+++ nw/tests/researchOnGoogle.test.js
@@ -2,6 +2,7 @@ module.exports = {
     'Search on google': (browser) => {
         browser
             .init()
+            .longCommand()
             .page.google().fillInSearchInput()
             .page.google().submit()
             .assert.containsText('#main', browser.globals.movieName)
